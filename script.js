9542cae69788353c3c9a45867689049a1a0d4445
Douglas Nyberg <douglas.nyberg@outlook.com>
2024-08-07 01:11:08 +0200

    added some functionality to the interface

--- script.js
+++ script.js
@@ -8,10 +8,37 @@ let timer = 30000;
 let record = 0;
 let language = languageElement.value;
 let difficulty = difficultyElement.value;
-console.log(difficulty)
+
+let englishWords = [];
+let swedishWords = [];
+
+const getEnglishWords = () => {
+  fetch("./src/englishWords.json")
+    .then((res) => res.json())
+    .then((data) => {
+      englishWords = data;
+    })
+    .catch((error) => console.error("Error loading English words: ", error));
+};
+
+const getSwedishWords = () => {
+  fetch("./src/swedishWords.json")
+    .then((res) => res.json())
+    .then((data) => {
+      swedishWords = data;
+      
+    })
+    .catch((error) => console.error("Error loading Swedish words: ", error));
+};
+
+const getRandomWord = async (language) => {
+  // HÄR FELAR DET!!!!
+  // Den returnerar undefined
+  return language[Math.floor(Math.random() * language.length)]; 
+};
 
 const changeTimer = () => {
-    const currentTimeValue = timerElement.innerHTML;
+  const currentTimeValue = timerElement.innerHTML;
   if (currentTimeValue === "30") {
     timer = 60000;
     timerElement.innerHTML = "60";
@@ -27,17 +54,27 @@ const changeTimer = () => {
 };
 
 const updateDifficulty = () => {
-    asdas
+  difficulty = difficultyElement.value;
+  updateWord();
+};
 
-    if (difficultyElement.innerHTML == "Easy") {
-        difficulty = "easy";
-        console.log("here")
-    }
-    if (difficultyElement.innerHTML == "Medium") {
-        difficulty = "medium";
-    }
-    if (difficultyElement.innerHTML == "Hard") {
-        difficulty = "hard";
-    }
-    console.log(difficulty);
-}
+const updateLanguage = () => {
+  language = languageElement.value;
+  updateWord();
+};
+
+const updateWord = () => {
+  if (language == "English") {
+    console.log(englishWords)
+    word.innerHTML = getRandomWord(englishWords);
+  }
+  if (language == "Swedish") {
+    word.innerHTML = getRandomWord(swedishWords);
+  }
+};
+
+inputField.addEventListener("input", function () {});
+
+getEnglishWords();
+getSwedishWords();
+updateWord();
